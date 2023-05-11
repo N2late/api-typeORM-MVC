ab57b3c4cd64bdb86b02ed40db1d28ca1f0e44dc
Tiago Serra <t.serra@sportradar.com>
2023-05-11 09:42:03 +0200

    Add .env to gitignore file

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,13 @@
 import { ConnectionOptions } from 'typeorm';
+import 'dotenv/config';
 
 export const config: ConnectionOptions = {
   type: 'mysql',
   host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'password',
-  database: 'dbbe',
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
   synchronize: true,
   logging: false,
   entities: ['src/entity/**/*.ts'],
